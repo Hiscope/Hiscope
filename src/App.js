e51464d83f749c7cb9ecc274d1c94ff193cc32fd
Hiscope <91071992+Hiscope@users.noreply.github.com>
2021-12-01 16:37:00 +1000

    т
    о

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import Home from './components/Home/home'
+import React from "react";
+
+
+function App() {
+  return (
+      <div className='App'>
+        <Home/>
+      </div>
+  );
+}
+
+export default App;
